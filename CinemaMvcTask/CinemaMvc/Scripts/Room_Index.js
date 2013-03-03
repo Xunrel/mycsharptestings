de0d4cbdd943a9d91b0778fb3343f1ab3881e68d
d.trott87@googlemail.com <d.trott87@googlemail.com@b31248b0-9531-557d-5742-3aa2c9fb92e4>
2013-03-03 10:56:26 +0000

    Modified JavaScript for different Actions (seperated add and remove persons)

--- CinemaMvcTask/CinemaMvc/Scripts/Room_Index.js
+++ CinemaMvcTask/CinemaMvc/Scripts/Room_Index.js
@@ -5,41 +5,89 @@
             'Couldn\'t add or remove persons' +
             '</div>';
 
-        $('.addRemoveBtn').bind('click', function () {
+        //$('.addRemoveBtn').bind('click', function () {
+        //    var $thisBtn = $(this);
+
+        //    try {
+        //        $thisBtn.preventDefault();
+        //    } catch(e) {
+
+        //    } 
+        //    var roomId = $thisBtn.prevAll('input#item_RoomId').val();
+        //    var seatsValue = $thisBtn.prevAll('input.addRemoveInput').val();
+
+        //    $.ajax({
+        //        url: '/Room/AddRemovePersons',
+        //        type: 'POST',
+        //        data: {
+        //            id: roomId,
+        //            count: seatsValue
+        //        },
+        //        success: function() {
+        //            window.location.href = window.location.href;
+        //        },
+        //        error: function () {
+        //            var $parentDiv = $thisBtn.parent();
+        //            $parentDiv.append(alertBox);
+        //        }
+        //    });
+        //});
+
+        $('.addBtn').bind('click', function () {
             var $thisBtn = $(this);
 
             try {
                 $thisBtn.preventDefault();
             } catch(e) {
 
-            } 
+            }
+
             var roomId = $thisBtn.prevAll('input#item_RoomId').val();
             var seatsValue = $thisBtn.prevAll('input.addRemoveInput').val();
 
+            makeAjaxCall($thisBtn, '/Room/AddPersons', roomId, seatsValue);
+        });
+
+        $('.removeBtn').bind('click', function () {
+            var $thisBtn = $(this);
+
+            try {
+                $thisBtn.preventDefault();
+            } catch (e) {
+
+            }
+
+            var roomId = $thisBtn.prevAll('input#item_RoomId').val();
+            var seatsValue = $thisBtn.prevAll('input.addRemoveInput').val();
+
+            makeAjaxCall($thisBtn, '/Room/RemovePersons', roomId, seatsValue);
+        });
+
+        var makeAjaxCall = function (sender, url, id, count) {
             $.ajax({
-                url: '/Room/AddRemovePersons',
+                url: url,
                 type: 'POST',
                 data: {
-                    id: roomId,
-                    count: seatsValue
+                    id: id,
+                    count: count
                 },
                 success: function() {
                     window.location.href = window.location.href;
                 },
                 error: function () {
-                    var $parentDiv = $thisBtn.parent();
+                    var $parentDiv = sender.parent();
                     $parentDiv.append(alertBox);
                 }
             });
-        });
+        };
 
-        $('.addRemoveInput').each(function() {
-            var $thisInput = $(this);
-            $thisInput.keyup(function(event) {
-                if (event.keyCode == 13) {
-                    $thisInput.next().click();
-                }
-            });
-        });
+//        $('.addRemoveInput').each(function() {
+//            var $thisInput = $(this);
+//            $thisInput.keyup(function(event) {
+//                if (event.keyCode == 13) {
+//                    $thisInput.next().click();
+//                }
+//            });
+//        });
     });
 })(jQuery);
