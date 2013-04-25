968eea50a61068cb32a3cc117dc2444a31d202e4
d.trott87@gmail.com <d.trott87@gmail.com@b31248b0-9531-557d-5742-3aa2c9fb92e4>
2013-04-25 16:00:03 +0000

    Added deck.js for Presentation Presentation Area included

---
+++ JsCodeConventions/JsCodeConventions/Areas/Presentation/Controllers/PresentationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JsCodeConventions.Areas.Presentation.Controllers
+{
+    public class PresentationController : Controller
+    {
+        //
+        // GET: /Presentation/Presentation/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ JsCodeConventions/JsCodeConventions/Areas/Presentation/PresentationAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace JsCodeConventions.Areas.Presentation
+{
+    public class PresentationAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Presentation";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Presentation_default",
+                "Presentation/{controller}/{action}/{id}",
+                new { controller = "Presentation", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
