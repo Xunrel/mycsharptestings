857895e06c909b5dbb594a8adafb0ac25d346620
d.trott87@gmail.com <d.trott87@gmail.com@b31248b0-9531-557d-5742-3aa2c9fb92e4>
2013-05-29 13:58:17 +0000

    Canvas tests

---
+++ trunk/html5tests/js/canvas.js
@@ -0,0 +1,27 @@
+(function() {
+	var ctx = document.getElementById('html5canvas')
+						.getContext('2d');
+	ctx.fillStyle = '#E34C26';
+	ctx.beginPath();
+	ctx.moveTo(39, 250);
+	ctx.lineTo(17, 0);
+	ctx.lineTo(262, 0);
+	ctx.lineTo(239, 250);
+	ctx.lineTo(139, 278);
+	ctx.fill();
+	
+	ctx.fillStyle = '#F06529';
+	ctx.beginPath();
+	ctx.moveTo(139, 257);
+	ctx.lineTo(139, 20);
+	ctx.lineTo(239, 20);
+	ctx.lineTo(220, 234);
+	ctx.fill();
+	
+	ctx.fillStyle = '#EBEBEB';
+	ctx.beginPath();
+	ctx.moveTo();
+	ctx.lineTo();
+	ctx.lineTo();
+	ctx.fill();
+})();
