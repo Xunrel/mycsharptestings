7ebee8dcb9e7b364e685e1da3fbe10c4213ba8b1
d.trott87@gmail.com <d.trott87@gmail.com@b31248b0-9531-557d-5742-3aa2c9fb92e4>
2013-05-29 09:56:56 +0000

    Initial RequireJsTests

---
+++ RequireJsTest/RequireJsTest/Scripts/app/entities/person.js
@@ -0,0 +1,14 @@
+﻿define(function () {
+    console.log('Person defining');
+    var Person = function(name, age) {
+        var self = this;
+        self.name = name;
+        self.arrayGetDistinctValues = age;
+    };
+
+    Person.prototype.getPersonInfo = function() {
+        return "Name: " + this.name + ", Age: " + this.age;
+    };
+
+    return Person;
+});
