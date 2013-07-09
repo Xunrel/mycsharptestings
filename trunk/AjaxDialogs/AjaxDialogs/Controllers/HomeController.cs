15bbe8d48fb9e64f4e03ffb055d9d5e9313874e6
d.trott87@gmail.com <d.trott87@gmail.com@b31248b0-9531-557d-5742-3aa2c9fb92e4>
2013-07-09 12:16:20 +0000

    Initial AjaxDialogs & Paging

---
+++ trunk/AjaxDialogs/AjaxDialogs/Controllers/HomeController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxDialogs.Models;
+
+namespace AjaxDialogs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public int PageSize = 5;
+
+        private readonly IEnumerable<string> _valuesRepository = new List<string>
+                                                                     {
+                                                                         "value 01",
+                                                                         "value 02",
+                                                                         "value 03",
+                                                                         "value 04",
+                                                                         "value 05",
+                                                                         "value 06",
+                                                                         "value 07",
+                                                                         "value 08",
+                                                                         "value 09",
+                                                                         "value 10",
+                                                                         "value 11",
+                                                                         "value 12",
+                                                                         "value 13",
+                                                                         "value 14",
+                                                                         "value 15",
+                                                                         "value 16",
+                                                                         "value 17",
+                                                                         "value 02",
+                                                                         "value 03",
+                                                                         "value 04",
+                                                                         "value 05",
+                                                                         "value 06",
+                                                                         "value 07",
+                                                                         "value 08",
+                                                                         "value 09",
+                                                                         "value 10",
+                                                                         "value 11",
+                                                                         "value 12",
+                                                                         "value 13",
+                                                                         "value 14",
+                                                                         "value 15",
+                                                                         "value 16",
+                                                                         "value 17",
+                                                                         "value 02",
+                                                                         "value 03",
+                                                                         "value 04",
+                                                                         "value 05",
+                                                                         "value 06",
+                                                                         "value 07",
+                                                                         "value 08",
+                                                                         "value 09",
+                                                                         "value 10",
+                                                                         "value 11",
+                                                                         "value 12",
+                                                                         "value 13",
+                                                                         "value 14",
+                                                                         "value 15",
+                                                                         "value 16",
+                                                                         "value 17",
+                                                                         "value 02",
+                                                                         "value 03",
+                                                                         "value 04",
+                                                                         "value 05",
+                                                                         "value 06",
+                                                                         "value 07",
+                                                                         "value 08",
+                                                                         "value 09",
+                                                                         "value 10",
+                                                                         "value 11",
+                                                                         "value 12",
+                                                                         "value 13",
+                                                                         "value 14",
+                                                                         "value 15",
+                                                                         "value 16",
+                                                                         "value 17"
+                                                                     };
+
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult GetValues()
+        {
+            return PartialView("_MyStringValues", _valuesRepository.ToList());
+        }
+
+        public PartialViewResult GetValue(int id = 0)
+        {
+            var model = _valuesRepository.Where(v => v.Contains(id.ToString(CultureInfo.InvariantCulture)));
+            return PartialView("_MyStringValues", model);
+        }
+
+        public ActionResult List(int page = 1)
+        {
+            var model = new ValueListViewModel
+                            {
+                                Values = _valuesRepository
+                                            .OrderBy(v => v)
+                                            .Skip((page - 1) * PageSize)
+                                            .Take(PageSize),
+                                PagingInfo = new PagingInfo
+                                                 {
+                                                     CurrentPage = page,
+                                                     ItemsPerPage = PageSize,
+                                                     TotalItems = _valuesRepository.Count()
+                                                 }
+                            };
+
+            return View(model);
+        }
+
+        public ActionResult AjaxList(int page = 1)
+        {
+            return PartialView("_ValueList", _valuesRepository
+                                                 .OrderBy(v => v)
+                                                 .Skip((page - 1)*PageSize)
+                                                 .Take(PageSize));
+        }
+    }
+}
---
+++ trunk/AjaxDialogs/AjaxDialogs/HtmlHelpers/PagingHelpers.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using AjaxDialogs.Models;
+
+namespace AjaxDialogs.HtmlHelpers
+{
+    /// <summary>
+    /// Paging helpers for default HTML ActionLink and Ajax ActionLink
+    /// </summary>
+    public static class PagingHelpers
+    {
+        /// <summary>
+        /// Helper to generate HTML ActionLink pagination based on <see cref="PagingInfo"/>.
+        /// </summary>
+        /// <param name="html"></param>
+        /// <param name="pagingInfo"></param>
+        /// <param name="pageUrl"></param>
+        /// <param name="htmlAttributes"></param>
+        /// <returns></returns>
+        public static MvcHtmlString PageLinks(this HtmlHelper html, PagingInfo pagingInfo, Func<int, string> pageUrl, object htmlAttributes = null)
+        {
+            var result = new StringBuilder();
+
+            for (var i = 1; i < pagingInfo.TotalPages; i++)
+            {
+                var tag = new TagBuilder("a");
+                tag.InnerHtml = i.ToString(CultureInfo.InvariantCulture);
+
+                if (i == pagingInfo.CurrentPage)
+                {
+                    tag.AddCssClass("selected");
+                }
+                if (htmlAttributes != null)
+                {
+                    var newAttributes = HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
+                    tag.MergeAttributes(newAttributes);
+                }
+                tag.MergeAttribute("href", pageUrl(i));
+                result.Append(tag.ToString());
+            }
+
+            return MvcHtmlString.Create(result.ToString());
+        }
+
+        /// <summary>
+        /// Helper to generate Ajax ActionLink pagination based on <see cref="PagingInfo"/>.
+        /// </summary>
+        /// <param name="ajax"></param>
+        /// <param name="pagingInfo"></param>
+        /// <param name="pageUrl"></param>
+        /// <param name="ajaxOptions"></param>
+        /// <param name="htmlAttributes"></param>
+        /// <returns></returns>
+        public static MvcHtmlString AjaxPageLinks(this AjaxHelper ajax, PagingInfo pagingInfo, Func<int, string> pageUrl,
+                                                  AjaxOptions ajaxOptions, object htmlAttributes = null)
+        {
+            var result = new StringBuilder();
+
+            for (var i = 1; i < pagingInfo.TotalPages; i++)
+            {
+                var tag = new TagBuilder("a");
+                tag.InnerHtml = HttpUtility.HtmlEncode(i.ToString(CultureInfo.InvariantCulture));
+
+                if (i == pagingInfo.CurrentPage)
+                {
+                    tag.AddCssClass("selected");
+                }
+
+                if (htmlAttributes != null)
+                {
+                    var newAttributes = HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
+                    tag.MergeAttributes(newAttributes);
+                }
+
+                tag.MergeAttribute("href", pageUrl(i));
+
+                if (ajax.ViewContext.UnobtrusiveJavaScriptEnabled)
+                {
+                    tag.MergeAttributes(ajaxOptions.ToUnobtrusiveHtmlAttributes());
+                }
+
+                result.Append(tag.ToString());
+            }
+
+            return MvcHtmlString.Create(result.ToString());
+        }
+    }
+}
