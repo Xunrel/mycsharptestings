bfe7c88bb0722e515ff5c6aaea05864211c06150
d.trott87@gmail.com <d.trott87@gmail.com@b31248b0-9531-557d-5742-3aa2c9fb92e4>
2013-08-20 10:08:01 +0000

    Initial commit

---
+++ trunk/AssemblyChecker/AssemblyChecker/AssemblyChecker.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyChecker.cs" company="busitec">
+//   Copyright (c) 2013 busitec GmbH. All rights reserved.
+// </copyright>
+// 
+// $Id$
+// 
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace AssemblyChecker
+{
+    public static class AssemblyChecker
+    {
+        public static bool IsAssemblyDebugBuild(string filepath)
+        {
+            return IsAssemblyDebugBuild(Assembly.LoadFile(Path.GetFullPath(filepath)));
+        }
+
+        private static bool IsAssemblyDebugBuild(Assembly assembly)
+        {
+            foreach (var attribute in assembly.GetCustomAttributes(false))
+            {
+                var debuggableAttribute = attribute as DebuggableAttribute;
+                if (debuggableAttribute != null)
+                {
+                    return debuggableAttribute.IsJITTrackingEnabled;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ trunk/AssemblyChecker/busitec.BuildChecker/ViewModels/AssemblyViewModel.cs
@@ -0,0 +1,28 @@
+﻿using busitec.BuildChecker.Models;
+
+namespace busitec.BuildChecker.ViewModels
+{
+    public class AssemblyViewModel
+    {
+        public AssemblyModel AssemblyModel { get; set; }
+        public bool IsDirectory { get; set; }
+        public bool IncludeSubDirectories { get; set; }
+
+        public void CheckAssemblies()
+        {
+            if (IsDirectory)
+            {
+                if (IncludeSubDirectories)
+                {
+                    
+                }
+            }
+            var result = CheckAssembly(AssemblyModel.PathToCheck);
+        }
+
+        private bool CheckAssembly(string filepath)
+        {
+            return AssemblyChecker.AssemblyChecker.IsAssemblyDebugBuild(filepath);
+        }
+    }
+}
