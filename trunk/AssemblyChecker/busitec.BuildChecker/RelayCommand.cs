db7d45c6e6781cdfcec07937f27e1b2620072c00
d.trott87@gmail.com <d.trott87@gmail.com@b31248b0-9531-557d-5742-3aa2c9fb92e4>
2013-08-21 15:01:48 +0000

    Added new classes for project

---
+++ trunk/AssemblyChecker/busitec.BuildChecker/RelayCommand.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace busitec.BuildChecker
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _onExecute;
+        private readonly Func<bool> _checkExecutable;
+
+        /// <summary>
+        /// Creates a RelayCommand which is always available.
+        /// </summary>
+        /// <param name="onExecute">The action to execute.</param>
+        public RelayCommand(Action onExecute)
+        {
+            _onExecute = onExecute;
+            _checkExecutable = () => true;
+        }
+
+        /// <summary>
+        /// Creates a relayCommand which will be check if the action is available before executing it.
+        /// </summary>
+        /// <param name="onExecute">The action to execute.</param>
+        /// <param name="checkExecutable">The condition whether the action is available.</param>
+        public RelayCommand(Action onExecute, Func<bool> checkExecutable)
+        {
+            _onExecute = onExecute;
+            _checkExecutable = checkExecutable;
+        }
+
+        /// <summary>
+        /// Defines the method that checks whether the action can be executed in its current state.
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            var action = _checkExecutable;
+            return action == null || action();
+        }
+
+        /// <summary>
+        /// Defines the method to executed when the command is invoked.
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            var action = _onExecute;
+            if (action != null)
+            {
+                action();
+            }
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
--- trunk/AssemblyChecker/busitec.BuildChecker/ViewModels/AssemblyViewModel.cs
+++ trunk/AssemblyChecker/busitec.BuildChecker/ViewModels/AssemblyViewModel.cs
@@ -1,22 +1,53 @@
-﻿using AssemblyChecker;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using AssemblyChecker;
+using busitec.BuildChecker.Annotations;
 using busitec.BuildChecker.Models;
 
 namespace busitec.BuildChecker.ViewModels
 {
-    public class AssemblyViewModel
+    public class AssemblyViewModel : INotifyPropertyChanged
     {
-        public AssemblyModel AssemblyModel { get; set; }
-        public bool IsDirectory { get; set; }
-        public bool IncludeSubDirectories { get; set; }
+        private bool _isDirectory;
 
-        public void CheckAssemblies()
+        public ICommand CheckAssembly { get; private set; }
+
+        public AssemblyModel AssemblyModel { get; set; }
+        public bool IncludeSubDirectories { get; set; }
+        public bool IsDirectory
+        {
+            get { return _isDirectory; }
+            set
+            {
+                _isDirectory = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public AssemblyViewModel()
+        {
+            AssemblyModel = new AssemblyModel();
+            CheckAssembly = new RelayCommand(() => CheckFile(AssemblyModel.PathToCheck));
+        }
+
+        public void CheckDirectory()
         {
             // TODO implement logic for directories, subdirectories and simple file check
         }
 
-        private bool CheckAssembly(string filepath)
+        public bool CheckFile(string filepath)
         {
             return AssemblyHelper.IsAssemblyDebugBuild(filepath);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
