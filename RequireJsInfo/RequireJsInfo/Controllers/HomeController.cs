aca398fd7afa9f9d07961623b07e90683e4c50a1
d.trott87@gmail.com <d.trott87@gmail.com@b31248b0-9531-557d-5742-3aa2c9fb92e4>
2013-10-08 09:49:44 +0000

    Initial commit

---
+++ RequireJsInfo/RequireJsInfo/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RequireJsInfo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Home/Details/5
+
+        public ActionResult Example1()
+        {
+            return View();
+        }
+    }
+}
